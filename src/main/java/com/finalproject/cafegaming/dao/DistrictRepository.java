b5a1988cfb6dd96aae703cb041e06beecc5df42a
AuPhan <aupv96@gmail.com>
2020-11-30 01:47:08 +0700

    update district

--- src/main/java/com/finalproject/cafegaming/dao/DistrictRepository.java
+++ src/main/java/com/finalproject/cafegaming/dao/DistrictRepository.java
@@ -1,8 +1,12 @@
 package com.finalproject.cafegaming.dao;
 
 import com.finalproject.cafegaming.model.District;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface DistrictRepository extends MongoRepository<District,String> {
 
+
+public interface DistrictRepository  extends MongoRepository<District,String>{
+    Page<District> findAllByIdLocation(String id, Pageable pageable);
 }
--- src/main/java/com/finalproject/cafegaming/service/district/DistrictServiceImp.java
+++ src/main/java/com/finalproject/cafegaming/service/district/DistrictServiceImp.java
@@ -11,80 +11,51 @@ import java.util.List;
 
 @Service
 public class DistrictServiceImp implements DistrictService{
+
+    final DistrictRepository districtRepository;
+
+    public DistrictServiceImp(DistrictRepository districtRepository) {
+        this.districtRepository = districtRepository;
+    }
+
+
+
     @Override
     public List<District> findAll(Pageable pageable) {
-        return null;
+        return districtRepository.findAll(pageable).getContent();
     }
 
     @Override
     public List<District> findAllByLocation(String id, Pageable pageable) {
-        return null;
+        return districtRepository.findAllByIdLocation(id, pageable).getContent();
     }
 
     @Override
     public Boolean save(District district) {
-        return null;
+        return districtRepository.save(district) instanceof District;
     }
 
     @Override
     public Boolean update(District district) {
-        return null;
+        District district1 =findById(district.getId());
+        district1.setCode(district.getCode());
+        district1.setName(district.getName());
+        district1.setIdLocation(district.getIdLocation());
+        district1.setStatus(district.getStatus());
+        district1.setUpdateAt(district.getUpdateAt());
+        return save(district1);
     }
 
     @Override
     public Boolean delete(String id) {
-        return null;
+        District district = findById(id);
+        district.setStatus("Not Active");
+        district.setUpdateAt(LocalDateTime.now());
+        return save(district);
     }
 
     @Override
     public District findById(String s) {
-        return null;
+        return districtRepository.findById(s).orElseThrow(ResourceException::new);
     }
-
-
-//    final DistrictRepository districtRepository;
-//
-//    public DistrictServiceImp(DistrictRepository districtRepository) {
-//        this.districtRepository = districtRepository;
-//    }
-//
-//    @Override
-//    public List<District> findAll(Pageable pageable) {
-//        return districtRepository.findAll(pageable).getContent();
-//    }
-//
-//    @Override
-//    public List<District> findAllByLocation(String id, Pageable pageable) {
-//        return districtRepository.findAllByLocation(id, pageable).getContent();
-//    }
-//
-//    @Override
-//    public Boolean save(District district) {
-//        return districtRepository.save(district) instanceof District;
-//    }
-//
-//    @Override
-//    public Boolean update(District district) {
-//        District district1 =findById(district.getId());
-//        district1.setCode(district.getCode());
-//        district1.setName(district.getName());
-//        district1.setIdLocation(district.getIdLocation());
-//        district1.setStatus(district.getStatus());
-//        district1.setUpdateAt(district.getUpdateAt());
-//        return save(district1);
-//    }
-//
-//    @Override
-//    public Boolean delete(String id) {
-//        District district = findById(id);
-//        district.setStatus("Not Active");
-//        district.setUpdateAt(LocalDateTime.now());
-//        return save(district);
-//    }
-//
-//    @Override
-//    public District findById(String s) {
-//        return districtRepository.findById(s).orElseThrow(ResourceException::new);
-//    }
-
 }
