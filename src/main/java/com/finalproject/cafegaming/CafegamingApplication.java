a418c7b23cd56383348c4fe1e0ff414d58f4f6ac
nhtr98 <nhattruong.it98@gmail.com>
2020-12-05 21:11:05 +0700

    Update CafegamingApplication.java

--- src/main/java/com/finalproject/cafegaming/CafegamingApplication.java
+++ src/main/java/com/finalproject/cafegaming/CafegamingApplication.java
@@ -1,13 +1,5 @@
 package com.finalproject.cafegaming;
 
-import com.intuit.ipp.core.Context;
-import com.intuit.ipp.core.ServiceType;
-import com.intuit.ipp.data.Customer;
-import com.intuit.ipp.data.EmailAddress;
-import com.intuit.ipp.exception.FMSException;
-import com.intuit.ipp.security.OAuth2Authorizer;
-import com.intuit.ipp.services.DataService;
-import org.apache.commons.lang.RandomStringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -18,7 +10,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @SpringBootApplication
 public class CafegamingApplication {
 
-    public static void main(String[] args) throws FMSException {
+    public static void main(String[] args) {
         SpringApplication.run(CafegamingApplication.class, args);
     }
 
